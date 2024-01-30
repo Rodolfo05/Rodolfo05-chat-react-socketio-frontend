829271650c021ef99ab495f74e0b9ff00bf3be04
Rodolfo05 <rodolfodiaz.005@gmail.com>
2024-01-29 21:08:45 -0300

    primer commit

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/socket.io": {
+        target: 'https://chat-react-socketio-backend-production.up.railway.app',
+        ws: true
+      }
+    }
+  }
+})
