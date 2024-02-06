66bd4f7dd0d87f8651585d5291c8df9317e34f74
Rodolfo05 <rodolfodiaz.005@gmail.com>
2024-02-05 22:13:44 -0300

    se quita texto de prueba

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -50,7 +50,6 @@ export const Login = () => {
 
                 <button onClick={goToChat} className='w-80 text-2xl text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg px-5 py-2.5 text-center me-2 mb-2'>Entrar</button>
 
-            <label>{userID} / hola</label>
             </div>
 
 
